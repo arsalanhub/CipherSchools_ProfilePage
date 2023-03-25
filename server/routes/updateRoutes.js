bd6ddbd0599d2d9746e3f46b2f7c4cd942b9bb22
arsalanhub <arsalanmohd237@gmail.com>
2023-03-25 10:46:56 +0530

    Change Password working

--- server/routes/updateRoutes.js
+++ server/routes/updateRoutes.js
@@ -1,10 +1,10 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 module.exports.details = (req, res) => {};
 
 module.exports.about = async (req, res) => {
   const { userId, about_me } = req.body;
-  console.log(userId, about_me)
   let updatedData = await User.findByIdAndUpdate(
     userId,
     {
@@ -19,6 +19,16 @@ module.exports.web = (req, res) => {};
 
 module.exports.personalInfo = (req, res) => {};
 
-module.exports.password = (req, res) => {};
+module.exports.password = async (req, res) => {
+    const { userId, cur_password, new_password } = req.body;
+    const userData = await User.findById(userId);
+    let passwordChk = await bcrypt.compare(cur_password, userData.password);
+    if(!passwordChk) return res.status(200).json({ data: [], msg: "Invalid Password" });
+    let encryptedPassword = await bcrypt.hash(new_password, 10);
+    let updatedData = await User.findByIdAndUpdate(userId, {
+        password: encryptedPassword
+    }, { new: true });
+    return res.json({ data: updatedData, msg: "Password Updated" });
+};
 
 module.exports.interest = (req, res) => {};
