bb689552a18a24ea02473d349f3ce7ff432ce338
arsalanhub <arsalanmohd237@gmail.com>
2023-03-25 10:37:12 +0530

    About me update working

--- server/routes/updateRoutes.js
+++ server/routes/updateRoutes.js
@@ -1,23 +1,24 @@
-module.exports.details = (req, res) => {
+const User = require("../models/userModel");
 
-}
+module.exports.details = (req, res) => {};
 
-module.exports.about = (req, res) => {
-    
-}
+module.exports.about = async (req, res) => {
+  const { userId, about_me } = req.body;
+  console.log(userId, about_me)
+  let updatedData = await User.findByIdAndUpdate(
+    userId,
+    {
+      about_me,
+    },
+    { new: true }
+  );
+  return res.status(200).json({ data: updatedData, msg: "Data Updated" });
+};
 
-module.exports.web = (req, res) => {
-    
-}
+module.exports.web = (req, res) => {};
 
-module.exports.personalInfo = (req, res) => {
-    
-}
+module.exports.personalInfo = (req, res) => {};
 
-module.exports.password = (req, res) => {
-    
-}
+module.exports.password = (req, res) => {};
 
-module.exports.interest = (req, res) => {
-    
-}
+module.exports.interest = (req, res) => {};
