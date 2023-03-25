831d9b0d61e902015386721631745454186878f0
arsalanhub <arsalanmohd237@gmail.com>
2023-03-25 09:17:07 +0530

    utils added

---
+++ server/utils/dropdownData.js
@@ -0,0 +1,15 @@
+export const HighestEducation = [
+  "Primary",
+  "Secondary",
+  "Higher Secondary",
+  "Graduation",
+  "Post Graduation",
+];
+
+export const Currently = [
+  "Schooling",
+  "College Student",
+  "Teaching",
+  "Job",
+  "Freelancing",
+];
