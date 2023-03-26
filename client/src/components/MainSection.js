b043825a4b8747eda6ce30a5e077faf982a20a27
arsalanhub <arsalanmohd237@gmail.com>
2023-03-26 16:24:56 +0530

    On the web section added

--- client/src/components/MainSection.js
+++ client/src/components/MainSection.js
@@ -5,7 +5,7 @@ import ProfileSection from './ProfileSection'
 export default function MainSection() {
   const [display, setDisplay] = useState(false);
   return (
-    <div style={{ backgroundColor: "#F2F5FA", width: "94%" }}>
+    <div style={{ backgroundColor: "#F2F5FA", width: "100%" }}>
         <ProfileSection />
         { display && <FollowersSection /> }
     </div>
--- client/src/components/UserDetails.js
+++ client/src/components/UserDetails.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutMe from './userDetails/AboutMe'
+import OnTheWeb from './userDetails/OnTheWeb'
 
 export default function UserDetails() {
   return (
     <div className="userDetailsWrapper">
         <AboutMe />
+        <OnTheWeb />
     </div>
   )
 }
